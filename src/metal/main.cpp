dbb9a17654527c76cb6846bd412cb63833410b92
ayano <kagurazaka-ayano@outlook.com>
2024-02-09 11:25:45 -0500

    remove dependencies

---
+++ src/metal/main.cpp
@@ -0,0 +1,10 @@
+#define NS_PRIVATE_IMPLEMENTATION
+#define MTL_PRIVATE_IMPLEMENTATION
+
+#include "Metal/Metal.hpp"
+#include <iostream>
+
+int main() {
+    std::cout << "hello world!";
+    return 0;
+}
