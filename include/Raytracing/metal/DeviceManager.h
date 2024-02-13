68a319e8c93bbda0356e72cc06c060139140fc74
ayano <kagurazaka-ayano@outlook.com>
2024-02-13 11:16:49 -0500

    started metal
    Took 37 minutes

---
+++ include/Raytracing/metal/DeviceManager.h
@@ -0,0 +1,41 @@
+/**
+ * @file DeviceManager.h
+ * @author ayano
+ * @date 2/13/24
+ * @brief
+ */
+
+#ifndef RAYTRACING_DEVICEMANAGER_H
+#define RAYTRACING_DEVICEMANAGER_H
+
+#include "Metal.hpp"
+
+class DeviceManager {
+public:
+	DeviceManager(const DeviceManager&) = delete;
+	DeviceManager& operator=(const DeviceManager&) = delete;
+	DeviceManager(DeviceManager&&) = delete;
+	DeviceManager& operator=(DeviceManager&&) = delete;
+
+	static DeviceManager& Instance() {
+		static DeviceManager instance;
+		return instance;
+	}
+
+	static NS::SharedPtr<MTL::Device> GetDevice() {
+		static NS::SharedPtr<MTL::Device> device = NS::TransferPtr(MTL::CreateSystemDefaultDevice());
+		return device;
+	}
+
+	static NS::SharedPtr<MTL::CommandQueue> GetCommandQueue() {
+		static NS::SharedPtr<MTL::CommandQueue> commandQueue = NS::TransferPtr(GetDevice()->newCommandQueue());
+		return commandQueue;
+	}
+
+
+private:
+	DeviceManager();
+};
+
+
+#endif // RAYTRACING_DEVICEMANAGER_H
---
+++ src/metal/DeviceManager.cpp
@@ -0,0 +1,8 @@
+/**
+ * @file DeviceManager.cpp
+ * @author ayano
+ * @date 2/13/24
+ * @brief
+ */
+
+#include "metal/DeviceManager.h"
